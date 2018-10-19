ceab972e5716a8836b7803dd2344b8d464b3f928
dinghy26 <dinghy26@gmail.com>
2018-10-19 18:43:23 -0400

    2 proyect !!

---
+++ tup-1.py
@@ -0,0 +1,34 @@
+film = {
+    "Finding Dory":[3,5],
+    "Bourne":[18,5],
+    "Tarzan":[15,5],
+    "Ghost Busters":[12,5]
+}
+
+while True:
+    choice = input("Hello what movie you like to watch? ").strip().title()
+
+    if choice in film:
+        age = int(input("How older are you? ").strip())
+
+        #Check user age
+        
+        if age >= film[choice][0]:
+
+            if film[choice][1]>0:
+                print("enjoy the movie")
+                film[choice][1] = film[choice][1]-1
+
+            else:
+                print("sorry no more tikets")
+
+
+        else:
+            print("you are to young to see that film")   
+
+
+
+    else:
+        print("We dont have that film...")
+
+
